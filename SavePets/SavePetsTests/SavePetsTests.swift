36c0a23624528e679736ac786ee252764a9dc96a
haeseoklee <haeseoklee.dev@gmail.com>
2021-04-11 15:04:23 +0900

    세이브 펫즈 첫번째 커밋

---
+++ SavePets/SavePetsTests/SavePetsTests.swift
@@ -0,0 +1,33 @@
+//
+//  SavePetsTests.swift
+//  SavePetsTests
+//
+//  Created by Haeseok Lee on 2021/04/11.
+//
+
+import XCTest
+@testable import SavePets
+
+class SavePetsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
