d499b9224e190eb994dad57bc288197101d8c47c
haeseoklee <haeseoklee.dev@gmail.com>
2021-05-04 18:59:06 +0900

    navigationController의 root를 HomeViewController로 세팅, window의 루트로 navigationController 세팅

--- SavePets/SavePets/Sources/SceneDelegate.swift
+++ SavePets/SavePets/Sources/SceneDelegate.swift
@@ -10,13 +10,24 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
+    var navigationController: UINavigationController?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        
+        self.updateRootToHomeViewController()
+        self.window?.rootViewController = self.navigationController
+        self.window?.makeKeyAndVisible()
+        
         guard let _ = (scene as? UIWindowScene) else { return }
+    }
+    
+    private func updateRootToHomeViewController() {
+        let mainStoryboard = UIStoryboard(name: Constants.Name.mainStoryboard, bundle: nil)
+        let homeViewController = mainStoryboard.instantiateViewController(withIdentifier: Constants.Identifier.homeViewController)
+        self.navigationController = UINavigationController(rootViewController: homeViewController)
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
