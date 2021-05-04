531ad38381da95ab04f81eda69e4f835f5023d43
haeseoklee <haeseoklee.dev@gmail.com>
2021-05-04 18:56:55 +0900

    Contants 파일 추가

---
+++ SavePets/SavePets/Resources/Constants/Constants.swift
@@ -0,0 +1,22 @@
+//
+//  Contants.swift
+//  SavePets
+//
+//  Created by Haeseok Lee on 2021/05/03.
+//
+
+import UIKit
+
+struct Constants {
+    struct Name {
+        static let mainStoryboard: String = "Main"
+    }
+    
+    struct Identifier {
+        static let homeViewController: String = "HomeViewController"
+        static let noseSelectionViewController: String = "NoseSelectionViewController"
+        static let nosePhotoShootViewController: String = "NosePhotoShootViewController"
+        static let dogProfileViewController: String = "DogProfileViewController"
+        static let ownerProfileViewController: String = "OwnerProfileViewController"
+    }
+}
