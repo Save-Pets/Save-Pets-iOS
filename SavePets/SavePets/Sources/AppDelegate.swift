fb070a6e7a5969d3ba04353c59fb4c928df7fc44
haeseoklee <haeseoklee.dev@gmail.com>
2021-05-06 01:40:19 +0900

    IQKeyboardManager 적용 (키보드 자동 up down 기능 적용)

--- SavePets/SavePets/Sources/AppDelegate.swift
+++ SavePets/SavePets/Sources/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import IQKeyboardManagerSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        IQKeyboardManager.shared.enable = true
+        IQKeyboardManager.shared.enableAutoToolbar = false
+        IQKeyboardManager.shared.shouldResignOnTouchOutside = true
+        
         return true
     }
 
