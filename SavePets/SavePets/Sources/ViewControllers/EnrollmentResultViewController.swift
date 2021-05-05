ab7feb18ae551c0d39ed1c4cf0f18374ce940f30
haeseoklee <haeseoklee.dev@gmail.com>
2021-05-06 03:58:21 +0900

    EnrollmentResultVC 로직 구현

--- SavePets/SavePets/Sources/ViewControllers/EnrollmentResultViewController.swift
+++ SavePets/SavePets/Sources/ViewControllers/EnrollmentResultViewController.swift
@@ -8,22 +8,43 @@
 import UIKit
 
 class EnrollmentResultViewController: UIViewController {
-
+    
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var dogImageView: UIImageView!
+    @IBOutlet weak var dogEnrollmentNumberLabel: UILabel!
+    @IBOutlet weak var dogNameLabel: UILabel!
+    @IBOutlet weak var dogBirthYearLabel: UILabel!
+    @IBOutlet weak var dogGenderLabel: UILabel!
+    @IBOutlet weak var homeButton: UIButton!
+    
+    // MARK: - Variables
+    
+    
+    // View Life Cycles
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.initializeEnrollmentResultViewController()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    // Functions
+    
+    private func popToHomeViewController() {
+        guard let homeViewController = self.navigationController?.viewControllers.filter({ $0 is HomeViewController}).first as? HomeViewController else {
+            return
+        }
+        
+        self.navigationController?.popToViewController(homeViewController, animated: true)
     }
-    */
-
+    
+    private func initializeEnrollmentResultViewController() {
+        self.dogImageView.roundUp(radius: 12)
+        self.homeButton.roundUp(radius: 12)
+    }
+    
+    @IBAction func homeButtonTouchUp(_ sender: UIButton) {
+        self.popToHomeViewController()
+    }
 }
