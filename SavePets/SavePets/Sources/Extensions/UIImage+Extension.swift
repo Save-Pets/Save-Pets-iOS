b5278fcd48e3c1ef4c48d6832242270697a5ad2b
haeseoklee <haeseoklee.dev@gmail.com>
2021-05-08 21:48:08 +0900

    UIImage 익스텐션으로 이미지 크롭하는 코드 추가

---
+++ SavePets/SavePets/Sources/Extensions/UIImage+Extension.swift
@@ -0,0 +1,65 @@
+//
+//  UIImage+Extension.swift
+//  SavePets
+//
+//  Created by Haeseok Lee on 2021/05/07.
+//
+
+import UIKit
+
+extension UIImage {
+    
+    func crop(to: CGSize) -> UIImage {
+        
+        guard let cgimage = self.cgImage else { return self }
+        
+        let contextImage: UIImage = UIImage(cgImage: cgimage)
+        
+        guard let newCgImage = contextImage.cgImage else { return self }
+        
+        let contextSize: CGSize = contextImage.size
+        
+        //Set to square
+        var posX: CGFloat = 0.0
+        var posY: CGFloat = 0.0
+        let cropAspect: CGFloat = to.width / to.height
+        
+        var cropWidth: CGFloat = to.width
+        var cropHeight: CGFloat = to.height
+        
+        if to.width > to.height { //Landscape
+            cropWidth = contextSize.width
+            cropHeight = contextSize.width / cropAspect
+            posY = (contextSize.height - cropHeight) / 2
+        } else if to.width < to.height { //Portrait
+            cropHeight = contextSize.height
+            cropWidth = contextSize.height * cropAspect
+            posX = (contextSize.width - cropWidth) / 2
+        } else { //Square
+            if contextSize.width >= contextSize.height { //Square on landscape (or square)
+                cropHeight = contextSize.height
+                cropWidth = contextSize.height * cropAspect
+                posX = (contextSize.width - cropWidth) / 2
+            }else{ //Square on portrait
+                cropWidth = contextSize.width
+                cropHeight = contextSize.width / cropAspect
+                posY = (contextSize.height - cropHeight) / 2
+            }
+        }
+        
+        let rect: CGRect = CGRect(x: posX, y: posY, width: cropWidth, height: cropHeight)
+        
+        // Create bitmap image from context using the rect
+        guard let imageRef: CGImage = newCgImage.cropping(to: rect) else { return self}
+        
+        // Create a new image based on the imageRef and rotate back to the original orientation
+        let cropped: UIImage = UIImage(cgImage: imageRef, scale: self.scale, orientation: self.imageOrientation)
+        
+        UIGraphicsBeginImageContextWithOptions(to, false, self.scale)
+        cropped.draw(in: CGRect(x: 0, y: 0, width: to.width, height: to.height))
+        let resized = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return resized ?? self
+    }
+}
