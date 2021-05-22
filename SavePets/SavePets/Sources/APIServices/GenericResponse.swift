55c9d73dc9bad507ccad823a9659f04be91e88b6
haeseoklee <haeseoklee.dev@gmail.com>
2021-05-23 02:40:44 +0900

    API 호출시 받아오는 결과를 담는 기본 모델 GenericResponse 구현

---
+++ SavePets/SavePets/Sources/APIServices/GenericResponse.swift
@@ -0,0 +1,23 @@
+//
+//  GenericResponse.swift
+//  SavePets
+//
+//  Created by Haeseok Lee on 2021/05/19.
+//
+
+import Foundation
+
+struct GenericResponse<T: Codable>: Decodable {
+    var data: T?
+    var message: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case message, data, error
+    }
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        self.data = try? values.decode(T.self, forKey: .data)
+        self.message = try? values.decode(String.self, forKey: .message)
+    }
+}
